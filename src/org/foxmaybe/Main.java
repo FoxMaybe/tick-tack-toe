c8ce1412a4d94441631f147c608ef585506ae46e
Fox Maybe <97665479+FoxMaybe@users.noreply.github.com>
2022-01-14 05:37:14 +0200

    add win??
    need fix it

--- src/org/foxmaybe/Main.java
+++ src/org/foxmaybe/Main.java
@@ -12,21 +12,37 @@ public class Main {
                 {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '}  //7 = [4][0], 8 = [4][2], 9 = [4][4]
         };
-        Integer Pos;
+        Integer Pos, PosX = 0, PosO = 0;
         Boolean WhoseMove = Boolean.TRUE;
         Scanner Scan = new Scanner(System.in);
 
+        PrintFrame(PlayingField);
 
         for(int i=0; i<9; i-=-1){
-            PrintFrame(PlayingField);
-            System.out.print("Enter your placement (1-9):");
+
+            System.out.print("\nEnter your placement (1-9):");
             Pos = Scan.nextInt();
             Integer Cord = Move(Pos);
-            PlayingField[Cord / 10][Cord % 10] = XOrO(WhoseMove);
-            WhoseMove = !WhoseMove;
+
+            if(PlayingField[Cord / 10][Cord % 10] == ' ') {
+                PlayingField[Cord / 10][Cord % 10] = XOrO(WhoseMove);
+                if(WhoseMove) PosX += Cord;
+                else PosO += Cord;
+                WhoseMove = !WhoseMove;
+            }
+            else{
+                System.out.println("\nOccupied cell, select another one");
+                i--;
+            }
+            if(PrintWin(PosX, PosO))
+                break;
+            if(i>=9){
+                System.out.println("Beaten by no one!");
+            }
+            PrintFrame(PlayingField);
         }
 
-        PrintFrame(PlayingField);
+
 
     }
 
@@ -35,6 +51,26 @@ public class Main {
             return 'x';
         else
             return 'o';
+    }
+    public static Boolean PrintWin(Integer PosX, Integer PosO){
+        if(WhoseWin(PosX)) {
+            System.out.println("X win is game!");
+            return Boolean.TRUE;
+        }
+        if(WhoseWin(PosO)){
+            System.out.println("X win is game!");
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
+    }
+
+    public static Boolean WhoseWin(Integer Pos){
+        Integer[] WinPos = {6,66,126,60,72};
+        for(Integer pos : WinPos){
+            if(Pos == pos)
+                return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
     }
 
     public static Integer Move(Integer Pos){
